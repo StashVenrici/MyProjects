3ed56c7279083f2fe3b8b0ce0a4b7ba984b7eacf
StashVenrici <newstash2012@gmail.com>
2018-04-08 23:40:29 +0300

    ДЗ5 - задание 1 готово

--- HW5_work_with_files/HW5_work_with_files/main.cpp
+++ HW5_work_with_files/HW5_work_with_files/main.cpp
@@ -1,27 +1,69 @@
 #include<iostream>
 #include<conio.h>
 #include<fstream>
+#include<cctype>
+#include<string>
 
 using namespace std;
 
 void GetTxtStatistic(const string txt_file);
 
-void GetTxtStatistic(const string txt_file)
+void GetTxtStatistic(const string txt_file = "test.txt")
 {
-	//создание текстового файла-отчета
-	ofstream fout;
-	fout.open("txt_report.txt");
+	char currentSymbol;
+	char vowels[] = "AEIOUYaeiouyАУОЕЫИЭЯЁЬЮауоеыиэяёью";  //список гласных букв для подсчета
+	int sym_count = 0,
+		lines_count = 1,
+		vowels_count = 0,
+		consonants_count = 0,
+		digits_count = 0;
 
-	if (!fout.is_open())
+	ofstream fout;
+	ifstream fin;
+	fout.open("txt_report.txt");
+	fin.open(txt_file);
+
+	if (!fout.is_open() || !fin.is_open())
 	{
-		cout << "Ошибка записи в файл!\n";
+		cout << "Ошибка !\n";
 	}
 	else
 	{
-		fout << "TODO: Статистика";
+
+		//вот здесь не считывает русские символы
+		while (fin.get(currentSymbol))
+		{
+			sym_count++;
+			if (currentSymbol == '\n') ++lines_count;
+			if (isalpha((unsigned char)currentSymbol)) {
+				if (strchr(vowels, (unsigned char)currentSymbol))
+				{
+					vowels_count++;
+				}
+				else
+				{
+					consonants_count++;
+				}
+			}
+			if (isdigit((unsigned char)currentSymbol)) ++digits_count;
+
+		}
+
+		//блок записи статистики в файл
+		fout << "Статистика:\n"
+			<< "\nКоличество символов: " << sym_count
+			<< "\nКоличество строк: " << lines_count
+			<< "\nКоличество гласных букв: " << vowels_count
+			<< "\nКоличество согласных букв: " << consonants_count
+			<< "\nКоличество цифр: " << digits_count;
+
+		cout << "Статистика по тексту записана в файл \"txt_report.txt\"";
 	}
-	
-	fout.close();	//закрываем файл
+
+
+
+	fout.close();
+	fin.close();
 }
 
 int main()
@@ -35,8 +77,12 @@ int main()
 	-Количество согласных букв;
 	-Количество цифр.*/
 
+	string txt_file;
+
+	cout << "Статистика символов в тексте. Введите путь к текстовому файлу: ";
+	getline(cin, txt_file);
+	GetTxtStatistic(txt_file);
 	
-	GetTxtStatistic("test");
 
 
 
